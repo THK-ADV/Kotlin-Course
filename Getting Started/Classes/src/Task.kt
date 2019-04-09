4719386c20ad9fe737ac3121c5c2fa2767960d89
Florian Herborn <florianherborn2293@t-online.de>
2019-04-09 14:24:25 +0200

    Some fixes

--- Getting Started/Classes/src/Task.kt
+++ Getting Started/Classes/src/Task.kt
@@ -1,5 +1,5 @@
 data class Address(val street: String, val houseNumber: String, val postalCode: Int, val city: String)
 
-class AddressBook(val addresses: MutableList<Address>) {
+data class AddressBook(val addresses: MutableList<Address>) {
     fun contains(address: Address) = addresses.contains(address)
 }
